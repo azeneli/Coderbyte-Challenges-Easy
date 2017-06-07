e6faf49b9c5c1140a3e10126ad703417d992482d
Anri Zeneli <anri@seccosquared.com>
2017-06-07 11:12:39 -0400

    first factorial question

--- js/first_factorial.js
+++ js/first_factorial.js
@@ -3,10 +3,14 @@
 //Example if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
 
 
-// function first_factorial(num){
+function first_factorial(num){
+	var factorial = 1;
+	for(var i = 1; i <= num; ++i){
+		factorial = factorial * i;
+	}	
+	return factorial;
+}
 
-
-	
-// }
-
+var num_factorial = first_factorial(16);
+console.log( num_factorial );
 
